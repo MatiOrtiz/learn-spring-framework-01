46f24ccc80642c3cf7e6ea58f5e30c207cebf1b2
Matías Nicolás Ortiz <88355010+MatiOrtiz@users.noreply.github.com>
2024-01-11 15:05:18 -0300

    Cdi

--- src/main/java/com/masterSpring/example04/CDILauncherApplication.java
+++ src/main/java/com/masterSpring/example04/CDILauncherApplication.java
@@ -5,12 +5,30 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.xml.crypto.Data;
 import java.util.Arrays;
 
+@Named
 class BusinessClass{
+    DataService dataService;
 
+    @Inject
+    public void setDataService(DataService dataService){
+        this.dataService= dataService;
+        System.out.println("Setter Injection");
+    }
+
+    public DataService getDataService(){
+        return dataService;
+    }
 }
 
+@Component
+class DataService{
+
+}
 
 @Configuration
 @ComponentScan
@@ -21,6 +39,8 @@ public class CDILauncherApplication {
         AnnotationConfigApplicationContext context=
                 new AnnotationConfigApplicationContext(CDILauncherApplication.class);
         Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
+        context.getBean(BusinessClass.class).getDataService();
 
     }
 
